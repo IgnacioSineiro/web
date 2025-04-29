ba7256cae6ef99ab8141d8d9d6ef5fd444c9984d
IgnacioSineiro <ignaciosineiro07@gmail.com>
2025-04-29 14:45:33 -0300

    Add files via upload

---
+++ qr.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import qrcode
+
+app = Flask(__name__)
+
+# Generar código QR con la URL del catálogo
+def generar_qr():
+    qr = qrcode.make("https://ignaciosineiro.github.io/web/")
+    qr.save("static/qrcode.png")
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    generar_qr()
+    app.run(debug=True)
